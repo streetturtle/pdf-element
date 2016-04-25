d5791afe0b3f0498ee08a32357c49192cdca6197
Pavel Makhov <pavel.makhov@cern.ch>
2016-04-25 17:57:30 +0200

    optional text selection, optional spinner

--- main.js
+++ main.js
@@ -9,9 +9,18 @@
     this.viewportOut = this.reader.querySelector('.pdf-viewport-out');
     this.viewport = this.reader.querySelector('.pdf-viewport');
     this.toolbar = this.reader.querySelector('.pdf-toolbar');
+    this.toolbarHeight = 64;
     this.title = this.toolbar.querySelector('.title');
-    this.textLayerDiv = this.reader.querySelector(".textLayer");
-    this.textLayerDivStyle = this.textLayerDiv.style;
+
+    this.enableTextSelection = el.enableTextSelection;
+
+    if (this.enableTextSelection){
+      this.textLayerDiv = this.reader.querySelector(".textLayer");
+      this.textLayerDivStyle = this.textLayerDiv.style;
+    }
+
+    this.spinner = this.reader.querySelector(".spinner");
+
 
     this.totalPages = this.reader.querySelector('#totalPages');
 
@@ -26,6 +35,8 @@
 
     this.setSize();
     this.loadPDF();
+    this.pageRendering = false;
+    this.pageNumPending = null;
   };
 
   Reader.prototype.setSize = function(attrName, newVal) {
@@ -44,31 +55,42 @@
     this.element.style.height = this.reader.style.height = height + 'px';
 
     this.viewportOutStyle.width = width + 'px';
-    this.viewportOutStyle.height = height - 64 + 'px';
+    this.viewportOutStyle.height = height - this.toolbarHeight + 'px';
+
+    this.spinner.style.top = (height - this.toolbarHeight) / 2 + 'px';
   };
 
   Reader.prototype.setSrc = function(src) {
     this.SRC = src;
   };
+
+  Reader.prototype.queueRenderPage = function(num) {
+    if (this.pageRendering) {
+      this.pageNumPending = num;
+    } else {
+      this.renderPDF(num);
+    }
+  };
+
   Reader.prototype.loadPDF = function() {
     var self = this;
 
     PDFJS.getDocument(this.SRC).then(function(pdf) {
       self.PDF = pdf;
-      self.renderPDF(1);
+      self.queueRenderPage(1);
 
       self.currentPage = 1;
       self.totalPages.innerHTML = self.PDF.numPages;
       self.totalPagesNum = self.PDF.numPages;
       self.currentZoomVal = self.fitZoomVal = self.widthZoomVal = 0;
       self.createDownloadLink();
-
     });
   };
 
   Reader.prototype.renderPDF = function(pageNum, resize) {
     var self = this;
-
+    self.pageRendering = true;
+    self.spinner.active = true;
     this.PDF.getPage(pageNum).then(function(page) {
       var scaleW, scaleH, viewerViewport, scale;
 
@@ -77,7 +99,7 @@
 
       if (self.currentZoomVal === 0 || !!resize) {
         scaleW = Math.round((self.WIDTH / self.pageW) * 100) / 100,
-          scaleH = Math.round(((self.HEIGHT - 64) / self.pageH) * 100) / 100,
+          scaleH = Math.round(((self.HEIGHT - self.toolbarHeight) / self.pageH) * 100) / 100,
           scale = Math.min(scaleH, scaleW);
         self.currentZoomVal = self.fitZoomVal = scale;
         self.widthZoomVal = self.WIDTH / self.pageW;
@@ -100,31 +122,39 @@
         self.viewport.height = self.pageH;
         self.viewportStyle.width = self.pageW + 'px';
         self.viewportStyle.height = self.pageH + 'px';
+        if (self.enableTextSelection){
+          self.textLayerDivStyle.width = self.pageW + 'px';
+          self.textLayerDivStyle.height = self.pageH + 'px';
+        }
+        // self.ctx.clearRect(0, 0, self.viewport.width, self.viewport.height);
 
-        // self.textLayerDiv.width = self.pageW;
-        // self.textLayerDiv.height = self.pageH;
-        // self.textLayerDiv.width = self.pageW + 'px';
-        // self.textLayerDiv.height = self.pageH + 'px';
-
-        self.ctx.clearRect(0, 0, self.viewport.width, self.viewport.height);
-
-        page.render({
+        var renderTask = page.render({
           canvasContext: self.ctx,
           viewport: viewerViewport
         });
-        self.textLayerDiv.innerHTML="";
-        page.getTextContent().then(function(textContent) {
 
-          self.textLayerDiv.setAttribute("style",self.viewport.getAttribute("style") + "top: " + self.viewportStyle.topNum + "px !important");
-          PDFJS.renderTextLayer({
-
-            textContent,
-            container: self.textLayerDiv,
-            pageIndex : pageNum,
-            viewport: viewerViewport,
-            textDivs: []
-          });
+        renderTask.promise.then(function () {
+          self.pageRendering = false;
+          self.spinner.active = false;
+          if (self.pageNumPending !== null) {
+            // New page rendering is pending
+            self.renderPDF(self.pageNumPending);
+            self.pageNumPending = null;
+          }
         });
+
+        if (self.enableTextSelection){
+          self.textLayerDiv.innerHTML="";
+          page.getTextContent().then(function(textContent) {
+            PDFJS.renderTextLayer({
+              textContent: textContent,
+              container: self.textLayerDiv,
+              pageIndex : pageNum,
+              viewport: viewerViewport,
+              textDivs: []
+            });
+          });
+        }
       }
     });
   };
@@ -132,11 +162,20 @@
   Reader.prototype.setViewportPos = function() {
     this.viewportStyle.left = (this.WIDTH - this.pageW) / 2 + 'px';
 
+    if (this.enableTextSelection)
+      this.textLayerDivStyle.left = (this.WIDTH - this.pageW) / 2 + 'px';
+
     if (this.pageH < this.HEIGHT) {
-      this.viewportStyle.top = (this.HEIGHT - this.pageH - 64) / 2 + 'px';
-      this.viewportStyle.topNum = Math.floor((this.HEIGHT - this.pageH - 64) / 2) + 64;
+      this.viewportStyle.top = (this.HEIGHT - this.pageH - this.toolbarHeight) / 2 + 'px';
+      this.viewportStyle.topNum = Math.floor((this.HEIGHT - this.pageH - this.toolbarHeight) / 2) + this.toolbarHeight;
+      if (this.enableTextSelection){
+        this.textLayerDivStyle.top = (this.HEIGHT - this.pageH - this.toolbarHeight) / 2 + 'px';
+        this.textLayerDivStyle.topNum = Math.floor((this.HEIGHT - this.pageH - this.toolbarHeight) / 2) + this.toolbarHeight;
+      }
     } else {
       this.viewportStyle.top = 0;
+      if (this.enableTextSelection)
+        this.textLayerDivStyle.top = 0;
     }
   };
 
@@ -148,29 +187,28 @@
   Reader.prototype.zoomIn = function() {
     var step = 0.1;
     this.currentZoomVal = Math.round((Math.round(this.currentZoomVal * 10) / 10 + step) * 10) / 10;
-    this.renderPDF(this.currentPage);
+    this.queueRenderPage(this.currentPage);
   };
 
   Reader.prototype.zoomOut = function() {
     var step = -0.1;
     this.currentZoomVal = Math.round((Math.round(this.currentZoomVal * 10) / 10 + step) * 10) / 10;
-    this.renderPDF(this.currentPage);
+    this.queueRenderPage(this.currentPage);
   };
 
   Reader.prototype.zoomPageFit = function() {
     this.currentZoomVal = this.fitZoomVal;
-    this.renderPDF(this.currentPage);
+    this.queueRenderPage(this.currentPage);
   };
 
   Reader.prototype.zoomWidthFit = function() {
     this.currentZoomVal = this.widthZoomVal;
-    this.renderPDF(this.currentPage);
+    this.queueRenderPage(this.currentPage);
   };
 
   Reader.prototype.getPageNum = function() {
-    console.log('asd' + this.PDF.numPages);
     return this.PDF.numPages;
-  }
+  };
 
   Reader.prototype.createDownloadLink = function() {
     var self = this;
